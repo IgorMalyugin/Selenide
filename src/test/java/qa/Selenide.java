ece7214136981ec71415d93ffb07bc5cd31d71ec
IgorMalyugin1 <90175900+IgorMalyugin1@users.noreply.github.com>
2023-06-14 23:53:08 +0300

    первое задание по selenide

---
+++ src/test/java/qa/Selenide.java
@@ -0,0 +1,23 @@
+package qa;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class Selenide {
+
+    @Test
+    public void lessen(){
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-body").shouldHave(Condition.text("Soft assertions"));
+        $(".markdown-body").$(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(Condition.text("Using JUnit5"));
+    }
+}
