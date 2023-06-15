555bd144afdf20d0effe62dcdec53ee167770e96
IgorMalyugin1 <90175900+IgorMalyugin1@users.noreply.github.com>
2023-06-15 18:14:25 +0300

    fix

--- src/test/java/qa/Selenide.java
+++ src/test/java/qa/Selenide.java
@@ -18,6 +18,16 @@ public class Selenide {
         $("#wiki-tab").click();
         $("#wiki-body").shouldHave(Condition.text("Soft assertions"));
         $(".markdown-body").$(byText("Soft assertions")).click();
-        $(".markdown-body").shouldHave(Condition.text("Using JUnit5"));
+        $(".markdown-body").shouldHave(Condition.text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
     }
 }
