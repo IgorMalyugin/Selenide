ea7355aa6d6fa5dc139ddd5ac27c683b93085be7
IgorMalyugin1 <90175900+IgorMalyugin1@users.noreply.github.com>
2023-06-17 23:59:11 +0300

    Решение Build like the best.

--- src/test/java/qa/Solution2.java
+++ src/test/java/qa/Solution2.java
@@ -1,2 +1,19 @@
-package qa;public class Solution2 {
+package qa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Solution2 {
+
+    @Test
+    public void gitHubSolutionsCheck(){
+        open("https://github.com/");
+        $(".header-menu-wrapper").$(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $(".enterprise-hero").shouldHave(text("Build like the best"));
+    }
 }
